c080ba2ce974b584013a6bb7a45a796c8982b829
igor_korolko <igor_korolko@alibaba-inc.com>
2021-11-21 22:33:09 +0300

    remove some

--- NotebookCollection/Model/CoreDataManager.swift
+++ NotebookCollection/Model/CoreDataManager.swift
@@ -15,8 +15,7 @@ class CoreDataManager {
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "NotebookCollection")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            storeDescription.shouldMigrateStoreAutomatically = true
-//            storeDescription.shouldInferMappingModelAutomatically = false
+
             
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
